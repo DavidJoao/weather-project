36a8f67adf430e456b7553711b8e71949921d489
DavidJoao <davidsandoval596@gmail.com>
2022-07-21 15:35:09 -0700

    added linkedin and github icons for contact card, need to work on media queries

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,15 @@
+import { Card } from 'react-bootstrap' 
+
 function Contact(){
     return(
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <Card className='contact-card' border="secondary" style={{ width: '18rem' }}>
+            <Card.Header>Contact Me!</Card.Header>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            <Card.Body>
+                <a href="https://www.linkedin.com/in/davidjoaosandoval/" target='_blank' className="fa fa-linkedin"></a>
+                <a href='https://github.com/DavidJoao' target='_blank' className="fa fa-github"> </a>
+            </Card.Body>
+      </Card>
     )
 }
 
