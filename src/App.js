2b1441792a2d11adf2940b2284c6853a800bbbdb
DavidJoao <davidsandoval596@gmail.com>
2022-07-22 09:17:28 -0700

    fixed navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import Navigation from './components/Navigation';
+import { Route, Routes, Link } from 'react-router-dom'
+import Input from './components/Input';
+import TimeZones from './components/TimeZones';
+import Contact from './components/Contact';
 
 function App() {
 
   return (
 
       <div className='App'>
-        <Navigation />
+        <nav className='navigation'>
+        <Link className='nav-link' to="/">
+          <h1 className='nav-text'>Weather</h1>
+        </Link>
+        <Link className='nav-link' to="/timezones">
+          <h1 className='nav-text'>Time Zone</h1>
+        </Link>
+        <Link className='nav-link' to='/contact'>
+          <h1 className='nav-text'>Contact</h1>
+        </Link>
+        </nav>
+        <main>
+        <Routes>
+            <Route path='/' element={ <Input /> } />
+            <Route path='/timezones' element={ <TimeZones /> } />
+            <Route path='/contact' element={ <Contact /> } />
+        </Routes>
+        </main>
       </div>
 
   );
